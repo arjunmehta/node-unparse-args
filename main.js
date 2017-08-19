3f705a6a38cd7373d69ad01f993c17eef1b61f88
Arjun Mehta <arjunmeht@gmail.com>
2017-08-19 12:58:39 -0700

    Cleanup code

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
 var shellescape = require('shell-escape');
 
 function unparse(parsed_args) {
-    var argv = [];
-    var flag;
-    var option;
-    var i;
+  var argv = [];
+  var flag;
+  var option;
+  var i;
 
-    for (option in parsed_args) {
-        if (option === '$0') {
-            continue;
-        }
-
-        flag = parsed_args[option];
-
-        if (option !== '_') {
-            if (testBoolean(flag) === false) {
-                argv.push('--no-' + option);
-            } else if (option.length === 1) {
-                argv.push('-' + option);
-            } else {
-                argv.push('--' + option);
-            }
-        }
-
-        if (testBoolean(flag) === true) {
-            continue;
-        } else {
-            if (Array.isArray(flag)) {
-                for (i = 0; i < flag.length; i++) {
-                    argv.push(flag[i]);
-                }
-            } else if (typeof flag === 'string' || typeof flag === 'number') {
-                argv.push('' + flag);
-            } else if (typeof flag === 'object') {
-                argv = argv.concat('[').concat(unparse(flag)).concat(']');
-            }
-        }
+  for (option in parsed_args) {
+    if (option === '$0') {
+      continue;
     }
 
-    Object.defineProperty(argv, 'command_string', {
-        enumerable: false,
-        get: commandString
-    });
+    flag = parsed_args[option];
 
-    return argv;
+    if (option !== '_') {
+      if (testBoolean(flag) === false) {
+        argv.push('--no-' + option);
+      } else if (option.length === 1) {
+        argv.push('-' + option);
+      } else {
+        argv.push('--' + option);
+      }
+    }
+
+    if (testBoolean(flag) === true) {
+      continue;
+    } else {
+      if (Array.isArray(flag)) {
+        for (i = 0; i < flag.length; i++) {
+          argv.push(flag[i]);
+        }
+      } else if (typeof flag === 'string' || typeof flag === 'number') {
+        argv.push('' + flag);
+      } else if (typeof flag === 'object') {
+        argv = argv.concat('[').concat(unparse(flag)).concat(']');
+      }
+    }
+  }
+
+  Object.defineProperty(argv, 'command_string', {
+    enumerable: false,
+    get: commandString
+  });
+
+  return argv;
 }
 
 
 function testBoolean(flag) {
-    if (flag === true || flag === 'true') {
-        return true;
-    } else if (flag === false || flag === 'false') {
-        return false;
-    }
+  if (flag === true || flag === 'true') {
+    return true;
+  } else if (flag === false || flag === 'false') {
+    return false;
+  }
 
-    return flag;
+  return flag;
 }
 
 function commandString() {
-    return shellescape(this);
+  return shellescape(this);
 }
 
 
--- test/test.js
+++ test/test.js
@@ -3,41 +3,41 @@ var subarg = require('subarg');
 var minimist = require('minimist');
 
 var fake_argv = [
-    'node',
-    '/Users/arjun/example/example.js',
-    'another',
-    '--number',
-    '297261',
-    '-t',
-    'something',
-    '-x',
-    'something longer than just 1 word',
-    '--no-bool',
-    '-e'
+  'node',
+  '/Users/arjun/example/example.js',
+  'another',
+  '--number',
+  '297261',
+  '-t',
+  'something',
+  '-x',
+  'something longer than just 1 word',
+  '--no-bool',
+  '-e'
 ];
 
 var fake_argv_withSubcontext = [
-    'node',
-    '/Users/arjun/example/example.js',
-    'another',
-    '--number',
-    '297261',
-    '-t',
-    'something',
-    '-x',
-    'something longer than just 1 word',
-    '--no-bool',
-    '-e',
-    '[',
-    'subcontext',
-    'here',
-    '-w',
-    'another',
-    '-a',
-    'something else',
-    '-b',
-    '276287',
-    ']'
+  'node',
+  '/Users/arjun/example/example.js',
+  'another',
+  '--number',
+  '297261',
+  '-t',
+  'something',
+  '-x',
+  'something longer than just 1 word',
+  '--no-bool',
+  '-e',
+  '[',
+  'subcontext',
+  'here',
+  '-w',
+  'another',
+  '-a',
+  'something else',
+  '-b',
+  '276287',
+  ']'
 ];
 
 var subarg_parsed = subarg(fake_argv_withSubcontext);
@@ -45,73 +45,73 @@ var minimist_parsed = minimist(fake_argv);
 var yargs_parsed = require('yargs').parse(fake_argv);
 
 exports['Exported Properly'] = function(test) {
-    test.expect(1);
-    test.equal(typeof unparse, 'function');
+  test.expect(1);
+  test.equal(typeof unparse, 'function');
 
-    test.done();
+  test.done();
 };
 
 exports['Unparse Subarg with SubContexts'] = function(test) {
-    var unparsed = unparse(subarg_parsed);
-    var i;
+  var unparsed = unparse(subarg_parsed);
+  var i;
 
-    test.expect(fake_argv_withSubcontext.length);
+  test.expect(fake_argv_withSubcontext.length);
 
-    for (i = fake_argv_withSubcontext.length - 1; i >= 0; i--) {
-        test.equal(fake_argv_withSubcontext[i], unparsed[i]);
-    }
+  for (i = fake_argv_withSubcontext.length - 1; i >= 0; i--) {
+    test.equal(fake_argv_withSubcontext[i], unparsed[i]);
+  }
 
-    test.done();
+  test.done();
 };
 
 exports['Unparse Yargs'] = function(test) {
-    var unparsed = unparse(yargs_parsed);
-    var i;
+  var unparsed = unparse(yargs_parsed);
+  var i;
 
-    test.expect(fake_argv.length);
+  test.expect(fake_argv.length);
 
 
-    for (i = fake_argv.length - 1; i >= 0; i--) {
-        test.equal(fake_argv[i], unparsed[i]);
-    }
+  for (i = fake_argv.length - 1; i >= 0; i--) {
+    test.equal(fake_argv[i], unparsed[i]);
+  }
 
-    test.done();
+  test.done();
 };
 
 exports['Unparse Minimist'] = function(test) {
-    var unparsed = unparse(minimist_parsed);
-    var i;
+  var unparsed = unparse(minimist_parsed);
+  var i;
 
-    test.expect(fake_argv.length);
+  test.expect(fake_argv.length);
 
-    for (i = fake_argv.length - 1; i >= 0; i--) {
-        test.equal(fake_argv[i], unparsed[i]);
-    }
+  for (i = fake_argv.length - 1; i >= 0; i--) {
+    test.equal(fake_argv[i], unparsed[i]);
+  }
 
-    test.done();
+  test.done();
 };
 
 exports['Minimist Command String'] = function(test) {
-    var unparsed = unparse(minimist_parsed);
-    var unparsed_command_string = unparsed.command_string;
+  var unparsed = unparse(minimist_parsed);
+  var unparsed_command_string = unparsed.command_string;
 
-    test.expect(1);
+  test.expect(1);
 
-    test.equal(unparsed_command_string, "node '/Users/arjun/example/example.js' another --number 297261 -t something -x 'something longer than just 1 word' --no-bool -e");
-    test.done();
+  test.equal(unparsed_command_string, "node '/Users/arjun/example/example.js' another --number 297261 -t something -x 'something longer than just 1 word' --no-bool -e");
+  test.done();
 };
 
 exports['Subarg Command String with SubContexts'] = function(test) {
-    var unparsed = unparse(subarg_parsed);
-    var unparsed_command_string = unparsed.command_string;
+  var unparsed = unparse(subarg_parsed);
+  var unparsed_command_string = unparsed.command_string;
 
-    test.expect(1);
+  test.expect(1);
 
-    test.equal(unparsed_command_string, "node '/Users/arjun/example/example.js' another --number 297261 -t something -x 'something longer than just 1 word' --no-bool -e '[' subcontext here -w another -a 'something else' -b 276287 ']'");
-    test.done();
+  test.equal(unparsed_command_string, "node '/Users/arjun/example/example.js' another --number 297261 -t something -x 'something longer than just 1 word' --no-bool -e '[' subcontext here -w another -a 'something else' -b 276287 ']'");
+  test.done();
 };
 
 
 exports.tearDown = function(done) {
-    done();
+  done();
 };
